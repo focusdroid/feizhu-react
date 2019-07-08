7275c6581d8742714ee28ae8baab14f800ac8693
focusdroid <840254112@qq.com>
2019-07-08 10:49:48 +0800

    每日签到完成，每日签到二级链接没有做

--- README.md
+++ README.md
@@ -7,4 +7,6 @@
     > 3. 点击首页的搜索栏，跳转到搜索页面
     > 4. 未登录的header存放目录，page/notLoggedHeader/NotLoggedHeader.js
     > 5. 搜索页面，page/notLoggedSearch/NotLoggedSearch.js 未登录和登录共用
+    > 6. 机票、酒店、火车票、汽车票、门票都在view/ticketing 
+    > 7. 出境游、境内游、周边游都在view/play
     
--- src/page/content/Content.js
+++ src/page/content/Content.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 
 export default class Content extends Component{
   render(){
-    return(
+    return (
       <Fragment>
         <div className='mt30 content'>
           {/*链接票务代售start*/}
@@ -44,18 +44,78 @@ export default class Content extends Component{
             </ul>
           </div>
           {/*链接票务代售end*/}
-        {/*  旅游签证周边游start*/}
+          {/*  旅游签证周边游start*/}
           <div className='travel'>
-            <div>
-              <ul>
+            <div className='travel-parent-ul'>
+              <ul className='travel-ul'>
                 <li>
-                  <Link to='/'></Link>
+                  <Link to='/'>
+                    <p>出境游</p>
+                    <p>走遍全球</p>
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/'>
+                    <p>境内游</p>
+                    <p className='line'>狂野西部</p>
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/'>
+                    <p>周边游</p>
+                    <p>乐享周末</p>
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/'>
+                    <p>飞猪购</p>
+                    <p>免税预购</p>
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/'>
+                    <p>签证/签注</p>
+                    <p>在线办理</p>
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/'>
+                    <p>境外上网</p>
+                    <p>信用免押</p>
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/'>
+                    <p>用车</p>
+                    <p>接送9折</p>
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/'>
+                    <p>全部</p>
+                    <p>更多推荐</p>
+                  </Link>
                 </li>
               </ul>
             </div>
           </div>
-        {/*  旅游签证周边游end*/}
+          {/*  旅游签证周边游end*/}
         </div>
+        {/*每日签到start*/}
+        <div className='sign-parent'>
+          <div className='sign disflex'>
+            <div className='disflex'>
+              <img className='sign-img-one' src="https://gw.alicdn.com/tfs/TB1JMPzOsbpK1RjSZFyXXX_qFXa-239-76.png_110x10000.jpg_.webp" alt=""/>
+              <p>专属福利天天领</p>
+            </div>
+            <div className='vertical'></div>
+            <div className='disflex'>
+              <img className='sign-img-two' src="https://gw.alicdn.com/tfs/TB17LZBCb9YBuNjy0FgXXcxcXXa-224-100.png_110x10000.jpg_.webp" alt=""/>
+              <p>每日签到</p>
+            </div>
+          </div>
+        </div>
+        {/*每日签到end*/}
       </Fragment>
     );
   }
