e4edb1b5b6f44a3d804f74e568f676ae6d988823
focusdroid <840254112@qq.com>
2019-07-08 09:42:02 +0800

    更新文档，添加部分注释

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 ## rem问题
-1rem 约等于 20px
+    > 1rem 约等于 20px
+## 项目文件夹分布
+    > 1. 底部切换栏(footerBar)在components/footerBar/footerBar.js
+    其中底部五个子切换项在footerBarRouter中
+    > 2. 首页的内容（进入APP第一页），在page/content/Content.js中
+    > 3. 点击首页的搜索栏，跳转到搜索页面
+    > 4. 未登录的header存放目录，page/notLoggedHeader/NotLoggedHeader.js
+    > 5. 搜索页面，page/notLoggedSearch/NotLoggedSearch.js 未登录和登录共用
+    
--- src/page/content/Content.js
+++ src/page/content/Content.js
@@ -1,17 +1,60 @@
 import React, { Component, Fragment } from 'react'
 import './content.scss'
 import '../../style/css/base.css'
+import { Link } from 'react-router-dom'
 
 export default class Content extends Component{
   render(){
     return(
       <Fragment>
-        <div className='mt30'>
-          <ul>
-            <li>
-              <img src="" alt=""/>
-            </li>
-          </ul>
+        <div className='mt30 content'>
+          {/*链接票务代售start*/}
+          <div>
+            <ul className='disflex content-ul'>
+              <li>
+                <Link to='/'>
+                  <img src="//gw.alicdn.com/tfs/TB1JCcHCqmWBuNjy1XaXXXCbXXa-210-126.png_110x10000.jpg_.webp" alt=""/>
+                  <p>机票</p>
+                </Link>
+              </li>
+              <li>
+                <Link to='/'>
+                  <img src="//gw.alicdn.com/tfs/TB1s51ECuOSBuNjy0FdXXbDnVXa-210-126.png_110x10000.jpg_.webp" alt=""/>
+                  <p>酒店</p>
+                </Link>
+              </li>
+              <li>
+                <Link to='/'>
+                  <img src="//gw.alicdn.com/tfs/TB1BNE_wrZnBKNjSZFrXXaRLFXa-210-126.png_110x10000.jpg_.webp" alt=""/>
+                  <p>火车票</p>
+                </Link>
+              </li>
+              <li>
+                <Link to='/'>
+                  <img src="//gw.alicdn.com/tfs/TB1pN01wsUrBKNjSZPxXXX00pXa-210-126.png_110x10000.jpg_.webp" alt=""/>
+                  <p>汽车票</p>
+                </Link>
+              </li>
+              <li>
+                <Link to='/'>
+                  <img src="//gw.alicdn.com/tfs/TB1aJ4MwHArBKNjSZFLXXc_dVXa-210-126.png_110x10000.jpg_.webp" alt=""/>
+                  <p>门票</p>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          {/*链接票务代售end*/}
+        {/*  旅游签证周边游start*/}
+          <div className='travel'>
+            <div>
+              <ul>
+                <li>
+                  <Link to='/'></Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        {/*  旅游签证周边游end*/}
         </div>
       </Fragment>
     );
